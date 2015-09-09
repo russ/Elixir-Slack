68d2474502767f47a5082fc0f34c0e7c66d92582
Russ Smith <russ@bashme.org>
2015-09-09 16:03:22 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Slack.Mixfile do
 
   defp deps do
     [{:httpoison, "~> 0.7.0"},
-     {:exjsx, "~> 3.1.0"},
+     {:exjsx, "~> 3.2.0"},
      {:websocket_client, github: "jeremyong/websocket_client"},
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.6", only: :dev}]
